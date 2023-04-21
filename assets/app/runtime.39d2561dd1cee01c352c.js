1b3ca51a672dcb117f096a7fe4d43b569fab4a23
wongpratan <wongpratan@users.noreply.github.com>
2023-04-21 07:50:00 +0000

    Commit from GitHub Actions (Update Web Service)

--- assets/app/runtime.39d2561dd1cee01c352c.js
+++ assets/app/runtime.39d2561dd1cee01c352c.js
@@ -137,7 +137,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "" + chunkId + "." + {"js_webix_components_querybuilder_querybuilder_min_js":"640aca1b55d908b6919a","AppBuilder_ABFactory_js-node_modules_moment_locale_sync_recursive_":"3bf12d60d6c90f5bdf3a"}[chunkId] + ".js";
+/******/ 			return "" + chunkId + "." + {"js_webix_components_querybuilder_querybuilder_min_js":"640aca1b55d908b6919a","AppBuilder_ABFactory_js-node_modules_moment_locale_sync_recursive_":"a075a340b788fdd54107"}[chunkId] + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -343,4 +343,4 @@
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=runtime.26b88e79701f3836a61b.js.map
+//# sourceMappingURL=runtime.39d2561dd1cee01c352c.js.map
